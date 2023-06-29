d5dbaa944df11a747952d2ea70c9c58cbfcabb79
Mstislav Martynyuk <mmartynyuk@reunico.com>
2023-06-29 15:44:24 +0300

    Попытка заставить работать ProcessTestCoverage

--- src/test/java/com/cbr/bpm/WorkflowTest.java
+++ src/test/java/com/cbr/bpm/WorkflowTest.java
@@ -7,14 +7,20 @@ import com.cbr.bpm.service.OrderService;
 import com.cbr.bpm.service.WebsiteService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
+import org.camunda.community.process_test_coverage.junit4.platform7.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
@@ -26,7 +32,6 @@ import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-//@Import(ProcessEngineCoverageConfiguration.class)
 public class WorkflowTest extends AbstractProcessEngineRuleTest {
 
   @Autowired
@@ -40,6 +45,10 @@ public class WorkflowTest extends AbstractProcessEngineRuleTest {
 
   @MockBean
   CrmService crmService;
+
+  @Rule
+  @ClassRule
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
 
   @Before
   public void setup() {
@@ -55,6 +64,7 @@ public class WorkflowTest extends AbstractProcessEngineRuleTest {
     return order;
   }
   @Test
+  @Deployment(resources = {"partymaker.bpmn", "proposal.dmn"})
   public void shouldExecuteHappyPath() {
     // given
     String processDefinitionKey = "partymaker";
